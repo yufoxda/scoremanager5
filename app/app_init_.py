b5fd7a95be7a126c0d7cdd0dcf58f482cefefa95
kingfoxfriend <105112698+kingfoxfriend@users.noreply.github.com>
2024-09-04 13:35:21 +0900

    初期コミット

---
+++ app/app_init_.py
@@ -0,0 +1,17 @@
+from database.skima.createSQL import Book, Song, Author, PublishURL
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+# create the app
+app = Flask(__name__, template_folder="pages/templates",static_folder="pages/static")
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///../database/onpuscores.db'
+app.config['SQLALCHEMY_ECHO'] = True
+db = SQLAlchemy(app)
+
+per_page = 30
+
+import rootes
