43a678ac5b1846ab7fa48755490fe100c197c1b0
yufoxda <147848207+yufoxda@users.noreply.github.com>
2024-11-01 07:45:26 +0900

    詳細検索

--- App/app_init_.py
+++ App/app_init_.py
@@ -1,4 +1,4 @@
-from Database.Schema.schema import Book,Song,Lyricist,SongWriter,Arranger
+from Database.Schema.schema import Book,Song,Lyricist,SongWriter,Arranger,Artist
 from Database.Schema.schema import SongLyricistAssociation,SongWriterAssociation,SongArrangerAssociation
 
 from sqlalchemy import create_engine
@@ -6,10 +6,11 @@ from sqlalchemy.orm import sessionmaker
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+import os
 
 # create the app
 app = Flask(__name__, template_folder="../Src/Templates",static_folder="../Src/Static")
-import os
+
 db_path = os.path.join(os.getcwd(), 'Database/ompooscores.db')
 app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{db_path}'
 
@@ -17,5 +18,3 @@ app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
 
 per_page = 30
-
-import App.Rootes.rootes
